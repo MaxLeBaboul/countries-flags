1d79bbee5cabd6c5895e78591dabf353f65c8c27
Lee Maxwell <maxwellbaboula@gmail.com>
2023-07-12 03:51:26 +0100

    Build header right

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Logo from "./components/Logo";
+import Navigation from "./components/Navigation";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <h1>Good Job</h1>
-    </div>
+    <BrowserRouter>
+      <Logo />
+      <Navigation />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/About.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <br />
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        maiores nesciunt aperiam molestiae, ullam unde ipsa at, sit nemo vero
+        perspiciatis eaque. Dolor corporis quos, nihil porro, eius pariatur
+        consectetur ducimus placeat accusantium deleniti eaque distinctio quidem
+        facere quam consequuntur autem numquam repellat molestiae voluptatibus.
+        Vel odio earum nulla nam recusandae qui aliquam eos. Enim aperiam iusto
+        optio rem nihil iste dolorum error, voluptas adipisci velit, harum ex
+        voluptate aliquam sit, architecto est quam pariatur quidem repellendus
+        qui dolore vel odit saepe ab. Quidem, beatae soluta. Impedit maxime
+        sapiente vitae, minima, labore molestiae eius placeat rerum iusto ipsum
+        repellat totam?
+      </p>
+      <br />
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure quas
+        voluptatum sed? Voluptas quam temporibus mollitia tenetur earum
+        asperiores quos dicta, soluta laudantium? Blanditiis cupiditate
+        molestiae architecto facilis quibusdam omnis, quae ratione quidem sunt
+        accusantium voluptate, nihil velit. Tempore officiis nam nisi voluptates
+        voluptatem, dolores incidunt asperiores fuga commodi illum totam ducimus
+        soluta molestias dolore illo aperiam veritatis placeat. Vitae, harum
+        rem. Soluta ea doloremque quod iste nihil alias voluptate est, laborum
+        labore sed facilis magnam? Sit a reiciendis pariatur sapiente quidem
+        omnis dolorum error. Rem inventore ab qui consequatur eaque excepturi,
+        libero eum eius molestias illum. Nemo quo iusto sapiente laboriosam
+        facere. Impedit deleniti eaque modi optio dolorum natus qui ea,
+        quibusdam praesentium quos, fuga voluptatum consequatur voluptate
+        perferendis soluta. Ea quos deserunt nihil iure pariatur libero fugiat
+        deleniti hic placeat, sed iusto sequi maiores quod, minima alias ad! Sit
+        facere perferendis eaque voluptas magni architecto, officia ullam non
+        tenetur possimus fugiat placeat quidem, adipisci maxime quo porro
+        voluptatibus esse aliquid veniam ab error. Nemo quibusdam laboriosam,
+        nulla hic labore quam cum iusto esse aliquid odit nesciunt fugit
+        consectetur? Quidem, ratione! Voluptates tempore modi debitis iste nemo
+        consequuntur, odio voluptatem rerum voluptate pariatur blanditiis eos
+        ad! Hic, enim delectus.
+      </p>
+    </div>
+  );
+};
+
+export default About;
