06ee7cf000dfc38cb3194ef06a93eae84e28b907
Lee Maxwell <maxwellbaboula@gmail.com>
2023-07-13 00:07:54 +0100

    Good add the countries flags

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Countries from "../components/Countries";
 
 const Home = () => {
   return (
     <div>
-      <h1>Accueil</h1>
+      <Countries />
     </div>
   );
 };
